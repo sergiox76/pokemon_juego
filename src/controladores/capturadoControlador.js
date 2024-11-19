0155f1490bc98bfb4fb09ff946298de32df25e15
sergiox76 <sergioxx767xx@gmail.com>
2024-11-19 09:29:23 -0500

    cambion en controladorCapturado

--- src/controladores/capturadoControlador.js
+++ src/controladores/capturadoControlador.js
@@ -2,50 +2,27 @@ const { PokemonCapturado, Pokemon, Usuario } = require('../baseDatos');
 
 const capturarPokemon = async (req, res) => {
   try {
-    const capturado = await PokemonCapturado.create(req.body);
-    res.status(201).json({
-      mensaje: "Pokémon capturado exitosamente",
-      resultado: capturado,
-    });
+    const capturado = await Capturado.create(req.body);
+    res.status(201).json({ mensaje: "Pokemon capturado",resultado:capturado });
   } catch (error) {
-    res.status(400).json({ mensaje: error.message, resultado: null });
+    res.status(400).json({ mensaje: error.message,resultado:null });
   }
 };
+const Usuario = require('../modelos/usuario'); // Modelo de Usuario
+const Pokemon = require('../modelos/pokemon'); // Modelo de Pokémon
 
-
+// Función para listar todas las capturas
 const listarCapturas = async (req, res) => {
   try {
-   
-    const capturas = await PokemonCapturado.findAll({
-      include: [
-        {
-          model: Pokemon,
-          as: 'Pokemon', 
-          attributes: ['nombre'], 
-        },
-        {
-          model: Usuario,
-          as: 'Usuario',
-          attributes: ['nombre'], 
-        },
-      ],
-    });
-
-    
-    const resultado = capturas.map((captura) => ({
-      id: captura.id,
-      pokemon: captura.Pokemon.nombre,
-      usuario: captura.Usuario.nombre,
-      fechaCaptura: captura.createdAt, 
-    }));
-
-    res.status(200).json(resultado);
+    const capturado = await Capturado.findAll(); // Obtenemos todos los Pokémon de la base de datos
+    res.status(200).json({capturado }); // Enviamos los Pokémon como respuesta en formato JSON
   } catch (error) {
-    res.status(400).json({ mensaje: error.message, resultado: null });
+    console.error('Error al obtener los Pokémones capturados:', error);
+    res.status(500).json({ mensaje: 'Error interno del servidor', resultado: null });
   }
-};
+}
 
 module.exports = {
-  capturarPokemon,
   listarCapturas,
+  capturarPokemon
 };
